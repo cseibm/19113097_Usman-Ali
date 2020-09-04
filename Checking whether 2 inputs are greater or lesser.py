0b43a9b77749f743a16f7ce8d071a1aac48da328
cseibm <68462471+cseibm@users.noreply.github.com>
2020-09-04 09:10:28 +0530

    Update Checking whether 2 inputs are greater or lesser.py

--- Checking whether 2 inputs are greater or lesser.py
+++ Checking whether 2 inputs are greater or lesser.py
@@ -1,1 +1,8 @@
-
+a=int(input('Enter a digit:'))
+b=int(input('Enter a digit:'))
+if a>b:  #Condition for checking 'a' is greater
+  print(a,' is greater')
+elif a==b:  #Condition for checking both 'a' and 'b' are equal
+  print(a,' and ',b,'are equal')
+else:
+  print(b,' is greater')
