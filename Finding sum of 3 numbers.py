36869ccc50368f42800bb12af6ef2cd125595700
cseibm <68462471+cseibm@users.noreply.github.com>
2020-09-04 09:00:24 +0530

    Create Finding sum of 3 numbers.py

---
+++ Finding sum of 3 numbers.py
@@ -0,0 +1,5 @@
+a=int(input('Enter 1st number:')) #Accepts First value
+b=int(input('Enter 2nd number:')) #Accepts Second value
+c=int(input('Enter 3rd number:')) #Accepts Third value
+sum=a+b+c  # Math logic for finding sum of three numbers
+print(sum)
