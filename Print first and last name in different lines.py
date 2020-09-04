0a96b4a83b979647302832f9e67156c0109711ff
cseibm <68462471+cseibm@users.noreply.github.com>
2020-09-04 09:39:12 +0530

    Create Print first and last name in different lines.py

---
+++ Print first and last name in different lines.py
@@ -0,0 +1,4 @@
+first_name=input('Enter the first name:')
+last_name=input('Enter the last name:')
+print('First name is ',first_name)  #Prints the First name
+print('Last name is ',last_name)  #Prints the last name
