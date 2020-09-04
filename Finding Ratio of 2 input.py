a0b6e6d483e28af33c5760dae0aaac411b83ccf9
cseibm <68462471+cseibm@users.noreply.github.com>
2020-09-04 09:26:30 +0530

    Update Finding Ratio of 2 input.py

--- Finding Ratio of 2 input.py
+++ Finding Ratio of 2 input.py
@@ -1,1 +1,4 @@
-
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+ratio = int(num1/num2) # Math logic for ratio
+print('The ratio of', num1, 'and', num2,'is', ratio)
