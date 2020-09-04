e83108c10e5e4e0177deb85841ee3c4d3e53121a
cseibm <68462471+cseibm@users.noreply.github.com>
2020-09-04 08:49:40 +0530

    Update and rename Print statement with 2 variable to Print statement with 2 variable.py

---
+++ Print statement with 2 variable.py
@@ -0,0 +1,4 @@
+firstdigit=int(input('Enter the first number:')  #Accepts an integer
+seconddigit=int(input('Enter the second digit:') #Accepts second integer
+print(firstdigit)  #prints the first integer
+print(seconddigit)  #prints the second integer
